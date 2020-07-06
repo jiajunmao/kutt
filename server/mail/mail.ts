c9e7f2504c3e31357ee9f9fb67b8b387977fad03
jiajunmao <aaronmao.hangzhou@gmail.com>
2020-07-06 04:25:39 +0000

    updating nodemailer for o365

--- server/mail/mail.ts
+++ server/mail/mail.ts
@@ -8,15 +8,12 @@ import env from "../env";
 
 const mailConfig = {
   service: "Outlook365",
-  host: env.MAIL_HOST,
-  port: env.MAIL_PORT,
-  secureConnection: env.MAIL_SECURE,
   auth: {
     user: env.MAIL_USER,
     pass: env.MAIL_PASSWORD
   }
 };
-
+ s
 const transporter = nodemailer.createTransport(mailConfig);
 
 export default transporter;
